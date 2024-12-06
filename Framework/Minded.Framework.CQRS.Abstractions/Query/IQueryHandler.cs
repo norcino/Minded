c17df29416c35cb9524921fd2a7e8a9b400c391d
Manuel Salvatori <manuel.salvatori@gmail.com>
2024-12-06 14:40:18 +0000

    Upgraded to .net 8

--- Framework/Minded.Framework.CQRS.Abstractions/Query/IQueryHandler.cs
+++ Framework/Minded.Framework.CQRS.Abstractions/Query/IQueryHandler.cs
@@ -2,6 +2,11 @@
 
 namespace Minded.Framework.CQRS.Query
 {
+    /// <summary>
+    /// Generic interface for query handlers
+    /// </summary>
+    /// <typeparam name="TQuery">Query type</typeparam>
+    /// <typeparam name="TResult">Query result type</typeparam>
     public interface IQueryHandler<in TQuery, TResult> where TQuery : IQuery<TResult>
     {
         Task<TResult> HandleAsync(TQuery query);
